d11eb8c2d017a8525d8e74d05378dc51e7edc2d5
Schmollanski <bayermaurice@gmail.com>
2024-05-29 11:21:38 +0200

    Created maths.h header

---
+++ Maths.h
@@ -0,0 +1,36 @@
+#pragma once
+#define ohImDieThankYouForeva(...) for(__VA_ARGS__)
+#define fubukus ,
+#define KFPEmployee ;
+#define mathJumpscare int
+#define X_Potato ?
+#define TheForbiddenWah std::cout
+#define TheySaidICouldBecomeAnything_TakoMeshLaundryPouch <<
+#define TotallyShef_Apron >>
+#define Bonk_Bonk_Bonk std::string
+#define YATAAAAA_AAAAAAA std::endl
+#define BibooTheRizzlerSays std::cin
+#define Nyahallo :
+#define KAELAAAAAAAAAAAAAAA +=
+#define SSRBShoppingBasket +
+#define _35P ++
+#define MikoInMaguma ==
+#define AmericaYaaa =
+#define Hello_Hello_Hello_Hello <
+#define HairballKorone *
+#define momosuzunene {}
+#define nenechi void
+#define NENESealInsolatedBottle 0
+#define BEEGsmol 1
+#define FuwamocoRadio 2
+#define Nr1Teamate(...) main() {__VA_ARGS__}
+#define MioFaBaller "pelase enter the values for the "
+#define MioDayoooooo "first"
+#define MuyoyoyoMimimimimi "second"
+#define ScarletPoliceGhettoPatrol24Hours " matrice:\n"
+#define smileFumo ""
+#define cirnoStare " "
+#define Soap "Enter value for matrice"
+#define GuraDoesNotWearPants "["
+#define DinoGura "]["
+#define GiveYourKingABigHug "]: "
